d882b584d0ca5ead18ddb28bba0eb5b4c00ae992
ANJ-NISH <155297183+ANJ-NISH@users.noreply.github.com>
2024-11-26 18:11:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+
+document.addEventListener("DOMContentLoaded",
+						 (event)=>
+							 {
+								 let btn=document.getElementById("enterBtn");
+
+								 const hele=document.createElement('h1');
+
+								 const text=document.createTextNode('Entered Metaverse');
+
+								 hele.appendChild(text);
+
+								 btn.addEventListener("click", function()
+													  {
+														  let pele=document.getElementById("status");
+														  pele.textContent="";
+														  pele.appendChild(hele);
+													  })
+							 })
